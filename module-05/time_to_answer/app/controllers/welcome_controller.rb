0c191ac1505498e7f3955515d08b7252936ea22f
Wesley Maia <maia.weol@gmail.com>
2022-10-25 11:08:37 -0700

    Exercise 5.04: Create login admin/profile

--- module-05/time_to_answer/app/controllers/welcome_controller.rb
+++ module-05/time_to_answer/app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: "wesley", password: "123"
-  
+  # http_basic_authenticate_with name: "wesley", password: "123"
+
   def index
   end
 end
